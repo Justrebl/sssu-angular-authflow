f421404440d7f1b4b7077120421320e9972633d1
julienS <durieljulien@gmail.com>
2022-06-30 11:51:49 +0200

    Working Auth Flow : v0.1 with profile only for authenticated user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { MsalModule, MsalRedirectComponent, MsalGuard} from '@azure/msal-angular';
+import { PublicClientApplication, InteractionType } from '@azure/msal-browser';
+import { environment } from 'src/environments/environment';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MsalModule.forRoot( new PublicClientApplication({
+      auth: {
+        clientId: environment.MSAL.clientId,
+        authority: environment.MSAL.authority,
+        redirectUri: environment.MSAL.redirectUri
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: isIE, // Set to true for Internet Explorer 11
+      }
+    }), {
+        interactionType: InteractionType.Redirect,
+        authRequest : {
+          scopes: ['user.read']
+        }
+    }, null)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MsalGuard],
+  bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  envName : "DEV",
+  MSAL:{
+    clientId: '6ac25636-f735-46a2-ba87-d6550709e35c', // Application (client) ID from the app registration
+    authority: 'https://login.microsoftonline.com/651d0ce8-93a6-43ef-9001-bb540ea99448', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
+    redirectUri: 'http://localhost:4200'// This is your redirect URI
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
