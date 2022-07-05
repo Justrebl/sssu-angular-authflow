754b064974ed3bcc56bb71250916abe6b84e5fe0
Julien Str <durieljulien@gmail.com>
2022-07-05 15:12:57 +0200

    v1.0 : Changing URL to destination SWA

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   MSAL:{
     clientId: '6ac25636-f735-46a2-ba87-d6550709e35c', // Application (client) ID from the app registration
     authority: 'https://login.microsoftonline.com/651d0ce8-93a6-43ef-9001-bb540ea99448', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
-    redirectUri: 'https://green-plant-01417c703.1.azurestaticapps.net/profile', // This is your redirect URI,
-    postLogoutRedirectUri: 'https://green-plant-01417c703.1.azurestaticapps.net' // This is your postlogout redirect URI,
+    redirectUri: 'https://salmon-mushroom-0de56e303.1.azurestaticapps.net/', // This is your redirect URI,
+    postLogoutRedirectUri: 'https://salmon-mushroom-0de56e303.1.azurestaticapps.net/' // This is your postlogout redirect URI,
   }
 };
